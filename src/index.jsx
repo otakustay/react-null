37fe87d3c71fa98e6260f2f14647f428d2d2a1be
Gray Zhang <otakustay@gmail.com>
2018-05-22 22:57:08 +0800

    Use function component instead of class
    This can gain a little performance since react skips all life cycle methods on function component

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,8 @@
 
 import React from 'react';
 
-export default class Null extends React.Component {
-  render() {
-    return null;
-  }
-};
+const Null = () => null;
+
+Null.displayName = 'Null';
+
+export default Null;
